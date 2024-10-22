46ffab4bd0db7b18516c13970378d7a42a55f906
LoicTrigault <loic.trigault@gmail.com>
2024-10-22 12:55:39 +0200

    Complete the unit test Numfinder

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -1,11 +1,16 @@
 package tudelft.numfinder;
 
+
 public class NumFinder {
     private int smallest = Integer.MAX_VALUE;
     private int largest = Integer.MIN_VALUE;
 
     public void find(int[] nums) {
+
         for(int n : nums) {
+            System.out.println("n : "+n);
+            System.out.println("smallest : "+smallest);
+            System.out.println("largest : "+largest);
 
             if(n < smallest)
                 smallest = n;
--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -6,12 +6,15 @@ public class NumFinderMain {
         NumFinder nf = new NumFinder();
 
         // this works
-        // nf.find(new int[] {4, 25, 7, 9});
+        nf.find(new int[] {4, 25, 7, 9});
 
-        // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        // this crashes car on ne fait que comparer des valeurs qui sont < n
+        // pas de passage dans le else if
+        //nf.find(new int[] {4, 3, 2, 1});
 
-        System.out.println(nf.getLargest());
-        System.out.println(nf.getSmallest());
+        System.out.println("=======Result=======");
+        System.out.println("largest : "+nf.getLargest());
+        System.out.println("smallest : "+nf.getSmallest());
+
     }
 }
